b80a21cf0ae1f94fc5eae53acb5e473bd72236ab
anjuncc <anjuncc@163.com>
2018-11-19 20:15:37 +0800

    ok

---
+++ README.md
@@ -0,0 +1,2 @@
+### config server test
+http://127.0.0.1:8888/web-admin-feign-dev/master 
--- hello-spring-cloud-web-admin-feign/src/main/java/anjun/hello/spring/cloud/web/admin/feign/WebAdminFeignApp.java
+++ hello-spring-cloud-web-admin-feign/src/main/java/anjun/hello/spring/cloud/web/admin/feign/WebAdminFeignApp.java
@@ -2,12 +2,14 @@ package anjun.hello.spring.cloud.web.admin.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
 public class WebAdminFeignApp {
     public static void main(String[] args) {
         SpringApplication.run(WebAdminFeignApp.class, args);
